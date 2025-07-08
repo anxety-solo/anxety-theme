7def9f9ea44f023dda6e169f0d451979a6729724
anxety <YuukiSanNya@yandex.ru>
2025-07-08 14:02:36 +0900

    update slider style (fix gr4)

---
+++ javascript/fix-slider-gr4.js
@@ -0,0 +1,12 @@
+onUiLoaded(function() {
+    // Remove class from all range inputs
+    function removeRangeClass() {
+        document.querySelectorAll('input[type="range"]').forEach(input => {
+            input.classList.remove('svelte-pc1gm4');
+        });
+    }
+    // Run
+    removeRangeClass();
+
+    console.log('[Anxety-Theme]: Range input class remover loaded - Forge Slider Fixed.');
+});
