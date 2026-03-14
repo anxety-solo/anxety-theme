492dab6b99035ad04dac5c547664b1588f66faf6
anxety-solo <YuukiSanNya@yandex.ru>
2026-03-14 23:36:59 +0900

    fix CN-I

--- javascript/fix-slider-gr4.js
+++ javascript/fix-slider-gr4.js
@@ -1,11 +1,22 @@
 onUiLoaded(function () {
-    // Remove class from all range inputs
+    if (opts.at_disable_theme) return;
+
+    // Forge sliders - remove default styling class
     function removeRangeClass() {
         document.querySelectorAll('input[type="range"]').forEach((input) => {
             input.classList.remove('svelte-pc1gm4');
         });
     }
-    removeRangeClass();
 
-    console.log('[Anxety-Theme]: Range input class `svelte-pc1gm4` removed - Forge Slider Fixed');
+    // ControlNet Integrated sliders - remove default styling class
+    function removeCNRangeClass() {
+        document.querySelectorAll('input[type="range"], div[class*="range-"]').forEach((el) => {
+            el.classList.remove('svelte-17pocne');
+        });
+    }
+
+    removeRangeClass();
+    removeCNRangeClass();
+
+    console.log('[Anxety-Theme]: Range input classes `svelte-pc1gm4`, `svelte-17pocne` removed - Forge & CN Integrated Sliders Fixed');
 });
